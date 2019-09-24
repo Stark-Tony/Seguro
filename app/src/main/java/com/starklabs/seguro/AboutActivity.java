e9c089aaa9e7f4bdcedfacd3eeffad90e8cc6dbe
Stark <35381380+Stark-Tony@users.noreply.github.com>
2019-09-24 13:45:44 +0530

    About completed

--- app/src/main/java/com/starklabs/seguro/AboutActivity.java
+++ app/src/main/java/com/starklabs/seguro/AboutActivity.java
@@ -3,6 +3,9 @@ package com.starklabs.seguro;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+
+import com.google.android.material.textview.MaterialTextView;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -10,5 +13,6 @@ public class AboutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
     }
 }
