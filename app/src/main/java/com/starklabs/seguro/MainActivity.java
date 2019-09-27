7a14bb9a60da51b8907a64e0eb9f6fb8c090a45a
Stark <35381380+Stark-Tony@users.noreply.github.com>
2019-09-27 16:42:29 +0530

    Main activity nav drawer done

--- app/src/main/java/com/starklabs/seguro/MainActivity.java
+++ app/src/main/java/com/starklabs/seguro/MainActivity.java
@@ -1,14 +1,39 @@
 package com.starklabs.seguro;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private DrawerLayout mDrawerLayout;
+    private Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        mDrawerLayout= findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle= new ActionBarDrawerToggle(this, mDrawerLayout,mToolbar,R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        mDrawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(mDrawerLayout.isDrawerOpen(GravityCompat.START))
+        {
+            mDrawerLayout.closeDrawer(GravityCompat.START);
+        }
+        else
+        {
+            super.onBackPressed();
+        }
     }
 }
